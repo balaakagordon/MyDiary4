1dda33dd5ee2dcb248aff9686f0f1941c74d5eaf
Gordonbalaaka <balaakagordon@gmail.com>
2018-08-14 16:29:42 +0300

    Redirect new users to login and display input error messages.

--- mydiary/templates/mydiary.js
+++ mydiary/templates/mydiary.js
@@ -7,7 +7,6 @@ function addUser() {
     fetch('http://127.0.0.1:5000/auth/signup', {
         method:'POST',
         headers:{
-            'Origin': '/registration.html',
             'Accept': 'application/json',
             'Content-type':'application/json'
         },
@@ -15,27 +14,30 @@ function addUser() {
     })
     .then((res) => res.json())
     .then(function (data){
-        //let serverresponse = data["message"];
-        if(data["message"] == "Invalid input"){
+        if(data["message"] == "Invalid input") {
             let errormsg = data["error"];
-        } else if(data["message"] == "Registered Successfully!"){
-            window.location.href='./home.html'
-            //login();
+            document.getElementById('regmessage').innerHTML = errormsg;
+        } else if(data["message"] == "Registered Successfully!") {
+            window.location.href='./login.html'
+            //login(email, password);
+        } else if(data["message"] == "This user already exists!") {
+            document.getElementById('regmessage').innerHTML = data["message"];
         }
     })
 }
+
 
 function login(){
 
     let email = document.getElementById('mail').value;
     let password = document.getElementById('pword').value;
-
     fetch('http://127.0.0.1:5000/auth/login', {
         method:'POST',
         headers:{
+            'Accept': 'application/json',
             'Content-type':'application/json'
         },
-        body:json.dumps({"email": email, "password":password})
+        body:json.stringify({"email": email, "password":password})
     })
     //.then((res) => res.json())
     .then(regResponse(data))
