21e96d69af50580c46c1ee250dfb92f6b16ca230
Gordonbalaaka <balaakagordon@gmail.com>
2018-08-14 14:55:08 +0300

    Add flask-cors to app

--- mydiary/__init__.py
+++ mydiary/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_jwt_extended import (
     JWTManager
 )
@@ -9,6 +10,7 @@ from .db import MyDiaryDatabase
 
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['JWT_SECRET_KEY'] = 'secret'
 jwt = JWTManager(app)
