66f9116ef38fc6da1e5b690feddcc25cbf99aa0f
Gordonbalaaka <balaakagordon@gmail.com>
2018-08-29 15:26:17 +0300

    Add status codes

--- mydiary/pages.py
+++ mydiary/pages.py
@@ -11,25 +11,24 @@ from mydiary import app, app_db, now_time, jwt
 @app.route('/')
 def landingpage():
     """ This endpoint loads the landing page """
-    return render_template('index.html')
-    # return jsonify({"message": "welcome"}), 200
+    return render_template('index.html'), 200
 
 @app.route('/registration')
 def signuppage():
-    return render_template('registration.html')
+    return render_template('registration.html'), 200
 
 @app.route('/login')
 def loginpage():
-    return render_template('login.html')
+    return render_template('login.html'), 200
 
 @app.route('/home')
 def homepage():
-    return render_template('home.html')
+    return render_template('home.html'), 200
 
 @app.route('/edit')
 def editpage():
-    return render_template('edit.html')
+    return render_template('edit.html'), 200
 
 @app.route('/userprofile')
 def profilepage():
-    return render_template('profile.html')
+    return render_template('profile.html'), 200
